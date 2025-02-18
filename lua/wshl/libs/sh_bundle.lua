8916b1e867e46cd28dab5fb3c3cb3bad44f84def
Zaurzo <101999193+Zaurzo@users.noreply.github.com>
2025-02-18 02:16:21 -0500

    wip 5

--- lua/wshl/libs/sh_bundle.lua
+++ lua/wshl/libs/sh_bundle.lua
@@ -44,7 +44,7 @@ function WSHL.Bundle:GetLoadable(fileList)
         end
     end
 
-    return loadableList, n
+    return loadableList
 end
 
 function WSHL.Bundle:GetAbsolutePath(relativePath)
@@ -106,4 +106,138 @@ function WSHL.Bundle:GetAbsolutePath(relativePath)
 
         i = i + 1
     end
+end
+
+--[[--------------------------------------------------------
+    Vanilla Load Order
+
+    This is to account for addons that replace vanilla Lua
+    files to typically load a file quicker.
+-----------------------------------------------------------]]
+
+local vanillaLuaFiles = {}
+local vanillaLuaLoadOrder = {}
+
+do
+    local function storeFiles(directory)
+        local files, directories = file.Find(directory .. '/*', 'MOD')
+    
+        for k, fileName in ipairs(files) do
+            if string.sub(fileName, -4) == '.lua' then
+                vanillaLuaFiles[directory .. '/' .. fileName] = true
+            end
+        end
+    
+        for k, dir in ipairs(directories) do
+            storeFiles(directory .. '/' .. dir)
+        end
+    end
+    
+    storeFiles('lua')
+    storeFiles('gamemodes/base')
+end
+
+function WSHL.Bundle:IsFileNameVanilla(fileName)
+    return vanillaLuaFiles[fileName] or false
+end
+
+--
+-- Search for include() and require() calls in the file
+-- and grab the Lua files they are going to load
+--
+function WSHL.Bundle:GetIncludesFromFile(fileToRead, path)
+    path = path or 'MOD'
+
+    local fileData = file.Read(fileToRead, path)
+    if not fileData then return {} end
+
+    local order = {}
+    local fileCount = 0
+
+    local folder = string.GetPathFromFilename(fileToRead)
+
+    local function tryMatch(stringCapture)
+        local pattern = '(%w+)%s*%(%s*(%b' .. stringCapture .. ')%s*%)'
+
+        for funcName, relativePath in string.gmatch(fileData, pattern) do
+            local filePath
+
+            relativePath = string.sub(relativePath, 2, #relativePath - 1)
+
+            if funcName == 'include' then
+                filePath = folder .. relativePath
+
+                if not file.Exists(filePath, path) then
+                    filePath = 'lua/' .. relativePath
+                end
+            elseif funcName == 'require' then
+                filePath = 'lua/includes/modules/' .. relativePath .. '.lua'
+            end
+
+            if filePath and file.Exists(filePath, path) then
+                fileCount = fileCount + 1
+                order[fileCount] = filePath
+            end
+        end
+    end
+
+    tryMatch("''")
+    tryMatch('""')
+    tryMatch('[]')
+
+    return order
+end
+
+--
+-- Creates a copy of the provided file list, sorted in the Lua load order
+--
+function WSHL.Bundle:GetInLuaLoadOrder(fileList)
+    local sortedFileList = {}
+    local vanillaFileReplacements
+
+    if fileList then
+        for k, fileName in ipairs(fileList) do
+            if string.sub(fileName, -4) == '.lua' then
+                if vanillaLuaLoadOrder[fileName] then
+                    vanillaFileReplacements = table.ForceInsert(vanillaFileReplacements, fileName)
+                elseif self:IsLoadable(fileName) then
+                    table.insert(sortedFileList, fileName)
+                end
+            end
+        end
+    end
+
+    table.sort(sortedFileList)
+
+    if vanillaFileReplacements then
+        table.sort(vanillaFileReplacements, function(a, b)
+            return vanillaLuaLoadOrder[a] < vanillaLuaLoadOrder[b]
+        end)
+
+        for k, fileName in ipairs(vanillaFileReplacements) do
+            table.insert(sortedFileList, k, fileName)
+        end
+    end
+
+    return sortedFileList
+end
+
+do
+    local position = 1
+
+    vanillaLuaLoadOrder['lua/includes/init.lua'] = 1
+
+    local function add(fileName)
+        for k, filePath in ipairs(WSHL.Bundle:GetIncludesFromFile(fileName, 'MOD')) do
+            if not vanillaLuaLoadOrder[filePath] then
+                position = position + 1
+                vanillaLuaLoadOrder[filePath] = position
+            end
+        end
+    end
+
+    add('lua/includes/init.lua')
+    add('gamemodes/base/gamemode/init.lua')
+    add('gamemodes/base/gamemode/cl_init.lua')
+    add('gamemodes/base/gamemode/shared.lua')
 end
